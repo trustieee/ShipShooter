34ff847ae14b58ac81a90f66c447f7ffa02d0bf1
mariocatch <mcacciatore@edatadimensions.com>
2020-05-07 00:35:20 -0400

    Add start to wave system. Refactor enemy spawner

--- Assets/Scripts/GameManagerSystem.cs
+++ Assets/Scripts/GameManagerSystem.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,24 +11,35 @@ public class GameManagerSystem : MonoBehaviour
     {
         Menu,
         GameCountdown,
+        SpawnWave,
         GamePlay,
         GameDeath,
-        GamePause
+        GamePause,
+        WaveCleared
     }
 
     private Color _defaultColor;
+    private Vector3 _defaultPlayerPosition;
     private bool _isCountdownRunning;
+    private bool _isWaveClearedShowing;
     private PlayerController _playerController;
+    private Spawner _spawner;
+    private float _waveClearedTimer;
 
     public float CountdownAmount;
     public Text CountdownText;
     public GameState State;
+    public float WaveClearedDuration;
+    public Text WaveClearedText;
 
     // Start is called before the first frame update
     private void Start()
     {
+        _spawner = FindObjectOfType<Spawner>();
+        WaveClearedText.enabled = false;
         _playerController = FindObjectOfType<PlayerController>();
         _defaultColor = Camera.main.backgroundColor;
+        _defaultPlayerPosition = _playerController.transform.position;
         State = GameState.GameCountdown;
     }
 
@@ -36,44 +50,81 @@ public class GameManagerSystem : MonoBehaviour
             case GameState.Menu:
                 break;
             case GameState.GameCountdown:
+            {
                 if (!_isCountdownRunning)
                 {
                     _isCountdownRunning = true;
                     CountdownText.enabled = true;
                 }
-
-                if (_isCountdownRunning)
+                else
                 {
+                    _playerController.transform.position = _defaultPlayerPosition;
                     if (CountdownAmount > 0)
                     {
                         CountdownAmount -= Time.deltaTime;
-                        CountdownText.text = CountdownAmount.ToString();
+                        CountdownText.text = CountdownAmount.ToString(CultureInfo.InvariantCulture);
                         Camera.main.backgroundColor = Color.grey;
                     }
                     else
                     {
-                        CountdownAmount = 0;
+                        CountdownAmount = 5;
                         _isCountdownRunning = false;
                         CountdownText.enabled = false;
                         Camera.main.backgroundColor = _defaultColor;
-                        State = GameState.GamePlay;
+                        State = GameState.SpawnWave;
                     }
                 }
 
                 break;
+            }
             case GameState.GamePlay:
+            {
                 if (!_playerController.IsAlive)
                 {
                     State = GameState.GameDeath;
                 }
 
+                if (_spawner.Enemies.All(e => e.GetComponent<EnemyBehavior>().IsAlive == false))
+                {
+                    _spawner.Enemies.Clear();
+                    State = GameState.WaveCleared;
+                }
+
                 break;
+            }
             case GameState.GameDeath:
                 break;
             case GameState.GamePause:
                 break;
+            case GameState.WaveCleared:
+                if (!_isWaveClearedShowing)
+                {
+                    StartCoroutine(ShowWaveCleared());
+                }
+
+                break;
+            case GameState.SpawnWave:
+                _spawner.SpawnWave(1);
+                State = GameState.GamePlay;
+                break;
             default:
                 throw new ArgumentOutOfRangeException();
         }
+    }
+
+    private IEnumerator ShowWaveCleared()
+    {
+        _isWaveClearedShowing = true;
+        WaveClearedText.enabled = true;
+        _waveClearedTimer = 0;
+        while (_waveClearedTimer < WaveClearedDuration)
+        {
+            _waveClearedTimer += Time.deltaTime;
+            yield return null;
+        }
+
+        WaveClearedText.enabled = false;
+        _isWaveClearedShowing = false;
+        State = GameState.GameCountdown;
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,20 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public List<GameObject> Enemies;
     public GameObject Enemy;
     public int MaxEnemies = 1;
 
-    // Start is called before the first frame update
-    private void Start()
+    private void Start() => Enemies = new List<GameObject>(MaxEnemies);
+
+    public void SpawnWave(int waveNumber)
     {
         for (var i = 0; i < MaxEnemies; i++)
         {
             var position = new Vector2(transform.position.x + i * 2, transform.position.y);
-            Instantiate(Enemy, position, Quaternion.identity);
+            Enemies.Add(Instantiate(Enemy, position, Quaternion.identity));
         }
     }
-
-    // Update is called once per frame
-    private void Update() { }
 }
